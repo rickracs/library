c2c7fce06b673b19ea15df17c78658716eddb611
Razon <rracsko@gmail.com>
2021-12-25 12:53:24 +0100

    cleaned up code, no need to reset whole shelf

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-// Q - is this a good way to initialize variables? Can I do this inside the functions?
 let myLibrary = [];
 
 const shelf = document.querySelector(".shelf");
@@ -7,10 +5,13 @@ const addBook = document.querySelector(".add-book");
 
 //Add 3 books automatically, then display them and listen for clicks
 myLibrary.push(new Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 350, "read"));
+displayBooks(myLibrary[myLibrary.length - 1]);
 myLibrary.push(new Book("And then there were none", "Agatha Christie", 300, "read"));
+displayBooks(myLibrary[myLibrary.length - 1]);
 myLibrary.push(new Book("Sahara", "Clive Cussler", 450, "not read yet"));
+displayBooks(myLibrary[myLibrary.length - 1]);
 
-displayBooks();
+
 
 
 //book constructor
@@ -23,15 +24,8 @@ function Book(title, author, pages, read) {
 
 //change read status, called on click. HTML Text content changed in separate function
 Book.prototype.changeStatus = function () {
-    if (this.read == "read") {
-        this.read = "not read yet"
-        console.log(this);
-
-    }
-    else {
-        this.read = "read"
-        console.log(this);
-    }
+    if (this.read == "read") this.read = "not read yet"
+    else this.read = "read"
 }
 
 //adding a book, window prompt is placeholder, will add form
@@ -41,108 +35,27 @@ function addBookToLibrary() {
     let pages = window.prompt("Enter number of pages: ");
     let read = window.prompt("Enter read status (read/not read yet): ");
     myLibrary.push(new Book(title, author, pages, read));
-    displayBooks();
+    displayBooks(myLibrary[myLibrary.length - 1]);
 }
 
-
-//clear the html when doing changes to books, not sure if this is the most efficient way
-//Q - maybe there is a way to show only the last book added?
-function resetShelf() {
-    while (shelf.firstChild) {
-        shelf.removeChild(shelf.lastChild);
-    }
-}
-
-
-//Q - clearing the HTML and dispalying books, is there a way to skip already added books? maybe use something else instead of splice when removing?
-function displayBooks() {
-    resetShelf();
-
-    myLibrary.forEach((book, index) => {
-
-        let newBook = document.createElement("div");
-        newBook.classList.add("book");
-        let bookTitle = document.createElement("h3");
-        let bookAuthor = document.createElement("p");
-        let bookPages = document.createElement("p");
-        let bookRead = document.createElement("button");
-        let removeButton = document.createElement("button");
-
-        bookTitle.textContent = book.title;
-        bookAuthor.textContent = "by " + book.author;
-        bookPages.textContent = book.pages + " pages";
-        bookRead.textContent = book.read;
-        bookRead.setAttribute("data-id", index);
-        bookRead.classList.add("read-status");
-        if (book.read == "not read yet") {
-            bookRead.classList.add("not-read-yet");
-        }
-        removeButton.textContent = "X";
-        removeButton.setAttribute("data-id", index);
-        removeButton.classList.add("remove-book");
-
-        newBook.appendChild(bookTitle);
-        newBook.appendChild(bookAuthor);
-        newBook.appendChild(bookPages);
-        newBook.appendChild(bookRead);
-        newBook.appendChild(removeButton);
-        shelf.appendChild(newBook);
-    });
-    listenForClick();
-
-}
-
-//changing the status of the book, first in the object using a prototype function, then in the HTML
-function changeReadStatus(e) {
-    myLibrary[e.target.getAttribute("data-id")].changeStatus();
-    e.target.textContent = myLibrary[e.target.getAttribute("data-id")].read;
-    if (myLibrary[e.target.getAttribute("data-id")].read == "not read yet")
-    e.target.classList.add("not-read-yet")
-    else e.target.classList.remove("not-read-yet");
-}
-
-//remove a book from the array using splice
-function removeBookFromLibrary(e) {
-    myLibrary.splice(e.target.getAttribute("data-id"), 1);
-    displayBooks();
-}
-
-//function to listen for clicks on books, doesn't seem to work without function
-function listenForClick() {
-    let removeBook = document.querySelectorAll(".remove-book");
-    removeBook.forEach(book => {
-        book.addEventListener("click", removeBookFromLibrary);
-    })
-    let readStatus = document.querySelectorAll(".read-status");
-    readStatus.forEach(book => {
-        book.addEventListener("click", changeReadStatus);
-    })
-
-}
-
-addBook.addEventListener("click", addBookToLibrary);
-
-
-
-
-
-
-/////EXPERIMENT, NOT NEEDED NOW
-/*
-    newBook = document.createElement("div");
+function displayBooks(book) {
+    let newBook = document.createElement("div");
     newBook.classList.add("book");
-    bookTitle = document.createElement("h3");
-    bookAuthor = document.createElement("p");
-    bookPages = document.createElement("p");
-    bookRead = document.createElement("button");
-    removeButton = document.createElement("button");
+    let bookTitle = document.createElement("h3");
+    let bookAuthor = document.createElement("p");
+    let bookPages = document.createElement("p");
+    let bookRead = document.createElement("button");
+    let removeButton = document.createElement("button");
 
-    bookTitle.textContent = title;
-    bookAuthor.textContent = "by " + author;
-    bookPages.textContent = pages + " pages";
-    bookRead.textContent = read;
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = "by " + book.author;
+    bookPages.textContent = book.pages + " pages";
+    bookRead.textContent = book.read;
     bookRead.setAttribute("data-id", (myLibrary.length - 1));
-    bookRead.classList.add("read-status")
+    bookRead.classList.add("read-status");
+    if (book.read == "not read yet") {
+        bookRead.classList.add("not-read-yet");
+    }
     removeButton.textContent = "X";
     removeButton.setAttribute("data-id", (myLibrary.length - 1));
     removeButton.classList.add("remove-book");
@@ -153,5 +66,52 @@ addBook.addEventListener("click", addBookToLibrary);
     newBook.appendChild(bookRead);
     newBook.appendChild(removeButton);
     shelf.appendChild(newBook);
+    listenForClick();
+}
 
-    listenForClick();*/
+
+//reset data ID of the books after removing one
+function resetShelf(index) {
+    shelf.removeChild(shelf.children[index]);
+    let readStatus = document.querySelectorAll(".read-status");
+    readStatus.forEach((button, index) => {
+        button.setAttribute("data-id", index);
+    });
+    let removeBook = document.querySelectorAll(".remove-book");
+    removeBook.forEach((button, index) => {
+        button.setAttribute("data-id", index);
+    });
+
+}
+
+
+//changing the status of the book, first in the object using a prototype function, then in the HTML
+function changeReadStatus(e) {
+    myLibrary[e.target.getAttribute("data-id")].changeStatus();
+    e.target.textContent = myLibrary[e.target.getAttribute("data-id")].read;
+    if (myLibrary[e.target.getAttribute("data-id")].read == "not read yet")
+        e.target.classList.add("not-read-yet")
+    else e.target.classList.remove("not-read-yet");
+}
+
+const buttonsWithId = document.querySelectorAll("[data-id]");
+
+//remove a book from the array using splice
+function removeBookFromLibrary(e) {
+    myLibrary.splice(e.target.getAttribute("data-id"), 1);
+    resetShelf(e.target.getAttribute("data-id"));
+}
+
+//function to listen for clicks on books, need to call it after adding a book
+function listenForClick() {
+    let removeBook = document.querySelectorAll(".remove-book");
+    removeBook.forEach(book => {
+        book.addEventListener("click", removeBookFromLibrary);
+    })
+    let readStatus = document.querySelectorAll(".read-status");
+    readStatus.forEach(book => {
+        book.addEventListener("click", changeReadStatus);
+    })
+}
+
+addBook.addEventListener("click", addBookToLibrary);
